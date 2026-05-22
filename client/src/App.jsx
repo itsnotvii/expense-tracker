ced981f01c6584fa7fc8878358f12648b8399a31
itsnotvii <islandcity57@gmail.com>
2026-05-22 14:43:53 -0700

    build expense tracker frontend with mock data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,6 +44,8 @@ function App() {
     byCategory[e.category] = (byCategory[e.category] || 0) + <e className="amount"></e>
   })
 
+  
+
   return (
     <div className="min-h-screen bg-black text-white p-8">
       <h1 className="text-4xl font-bold mb-8">Expenses Tracker</h1>
